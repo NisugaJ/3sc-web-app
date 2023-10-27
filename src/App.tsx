00716b3658e9237c46291e4d8a6182269308bffa
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-27 11:51:08 +0100

    App() content cleared

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,8 @@ import './App.css'
 function App() {
 
   return (
-      <div>
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          <div className="shrink-0">
-            <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
-          </div>
-          <div >
-            <div className="text-xl font-medium">ChitChat</div>
-            <p className='text-base'>You have a new message!</p>
-          </div>
-        </div>
+      <div className="h-screen container mx-0 my-0 flex justify-center items-center px-4">
+       
       </div>
   )
 }
