c3ba843fbe83870904e52c835aab57a9c6aa821b
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-27 14:15:23 +0100

    configuring opacity into hexadecimal color code

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
       'white-primary': '#FFFFFF',
 
       'green-secondary': '#00ED71',
-      'pink-secondary': '#F402561A',
+      'pink-secondary': '#F402561A', // (https://www.jmiron.com/percent-to-hex-converter)
 
       'gray':{
         'dark': '#5B5B5B',
@@ -26,9 +26,9 @@ export default {
       'gradient-primary': 'linear-gradient(81deg, #A0FF1F 13.17%, #00ED71 86.83%)',
 
       'success': '#00ED71',
-      'success-light': '#00ED7133', // last 2 numbers for 20% opacity
+      'success-light': '#00ED7133', // last 2 numbers for 20% opacity (https://www.jmiron.com/percent-to-hex-converter)
       'error':'#F40256',
-      'error-light': '#F4025626' // last 2 numbers are for 20% opacity
+      'error-light': '#F4025626' // last 2 numbers are for 20% opacity (https://www.jmiron.com/percent-to-hex-converter)
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
