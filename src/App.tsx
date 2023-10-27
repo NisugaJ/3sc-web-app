0bd0c3e6375360bdd8a89f8f70d287093738a839
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-27 13:40:06 +0100

    added atom design architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import AppPrimaryButton from './elements/atoms/AppPrimaryButton'
 
 function App() {
 
   return (
       <div className="h-screen container mx-0 my-0 flex justify-center items-center px-4">
-       
+       <AppPrimaryButton>Submit</AppPrimaryButton>
       </div>
   )
 }
