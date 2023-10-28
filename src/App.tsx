471d6264d8ebf98593598a5e6dc8d2c2e26aa6cc
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-28 17:35:59 +0100

    removed container to keep it simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import AppPrimaryButton from './elements/atoms/AppPrimaryButton'
 import AppSecondaryButton from './elements/atoms/AppSecondaryButton'
+import { AppButtonStates } from './ts-types/AppButtonProps'
 
 function App() {
 
   return (
-      <div className="h-screen container mx-0 my-0 px-4 flex justify-center items-center space-x-2">
-       <AppPrimaryButton>Submit</AppPrimaryButton>
-       <AppSecondaryButton>Cancel</AppSecondaryButton>
+      <div className="h-screen mx-0 my-0 xy-center-children space-x-2">
+        <AppPrimaryButton variant={AppButtonStates.loading}>Submit</AppPrimaryButton>
+        <AppSecondaryButton variant={AppButtonStates.loading}>Cancel</AppSecondaryButton>
       </div>
   )
 }
