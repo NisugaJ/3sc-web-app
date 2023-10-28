761de3fc17ef91044c3720cc616e94c8459cd6ee
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-28 18:06:32 +0100

    disabling button when in loading state

--- src/elements/atoms/AppButton.tsx
+++ src/elements/atoms/AppButton.tsx
@@ -1,10 +1,10 @@
 import classNames from "classnames"
-import AppButtonProps from "../../ts-types/AppButtonProps"
+import AppButtonProps, { AppButtonStates } from "../../ts-types/AppButtonProps"
 
 const AppButton = (props: AppButtonProps) => {
     return (
         <button 
-            className={classNames('w-80 h-12 px-4 py-2 drop-shadow-sm font-bold text-sm break-words uppercase xy-center-children ', props.className)} 
+            className={classNames('w-80 h-12 px-4 py-2 drop-shadow-sm font-bold text-sm break-words uppercase xy-center-children ', props.className, {'btn-disabled' : props.variant === AppButtonStates.loading})} 
         >
            {props.children}
         </button>
