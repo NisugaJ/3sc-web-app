09fe6ad53f45dcf3088a73919a1432066050e2f5
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-28 20:44:15 +0100

    wrapped the App with QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'
 import AppPrimaryButton from './elements/atoms/AppPrimaryButton'
 import AppSecondaryButton from './elements/atoms/AppSecondaryButton'
-import { AppButtonStates } from './ts-types/AppButtonProps'
 
 function App() {
+  const queryClient: QueryClient = new QueryClient();
 
   return (
+    <QueryClientProvider client={queryClient}>
       <div className="h-screen mx-0 my-0 xy-center-children space-x-2">
-        <AppPrimaryButton variant={AppButtonStates.loading}>Submit</AppPrimaryButton>
-        <AppSecondaryButton variant={AppButtonStates.loading}>Cancel</AppSecondaryButton>
+        <AppPrimaryButton> Submit</AppPrimaryButton>
+        <AppSecondaryButton>Cancel</AppSecondaryButton>
       </div>
+    </QueryClientProvider>
   )
 }
 
