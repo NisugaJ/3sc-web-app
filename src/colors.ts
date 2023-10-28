0ea4ec0322a11a589b9c6a0c2c23a820092513c9
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-28 17:32:42 +0100

    moved colors config to src

---
+++ src/colors.ts
@@ -0,0 +1,24 @@
+export const colors = {
+
+    'green-primary': '#A0FF1F',
+    'pink-primary': '#F70087',
+    'black-primary': '#000000',
+    'white-primary': '#FFFFFF',
+  
+    'green-secondary': '#00ED71',
+    'pink-secondary': '#F402561A', // (https://www.jmiron.com/percent-to-hex-converter)
+  
+    'gray':{
+      'dark': '#5B5B5B',
+      'DEFAULT': '#C3C3C3',
+      'light': '#F8F8F8',
+    },
+  
+    'gradient-primary': 'linear-gradient(81deg, #A0FF1F 13.17%, #00ED71 86.83%)',
+  
+    'success': '#00ED71',
+    'success-light': '#00ED7133', // last 2 numbers for 20% opacity (https://www.jmiron.com/percent-to-hex-converter)
+    'error':'#F40256',
+    'error-light': '#F4025626' // last 2 numbers are for 20% opacity (https://www.jmiron.com/percent-to-hex-converter)
+  }
+  
