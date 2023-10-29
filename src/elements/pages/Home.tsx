adeac88454c383700689a666e02e065974c0d959
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-29 20:40:15 +0000

    change App component to Home

---
+++ src/elements/pages/Home.tsx
@@ -0,0 +1,21 @@
+
+import AppPrimaryButton from '../atoms/AppPrimaryButton'
+import AppSecondaryButton from '../atoms/AppSecondaryButton'
+import NomineeListDropDown from '../molecules/NomineeListDropDown';
+import AppInputElement from '../atoms/AppInputElement';
+
+function Home() {
+
+  return (
+      <div className="h-screen mx-0 my-0 xy-center-children space-x-4 space-y-5 flex-col">
+        <div className='space-y-3 '>
+          <NomineeListDropDown/>
+          <AppInputElement type="textarea" labelText="Reason" bottomLeftLabel='Max. 300 words' placeholder='Enter reason'/>
+          <AppPrimaryButton> Submit</AppPrimaryButton>
+          <AppSecondaryButton>Cancel</AppSecondaryButton>
+        </div>
+      </div>
+  )
+}
+
+export default Home
