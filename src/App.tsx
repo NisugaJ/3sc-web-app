9f59e64ec72c86a5bddf5ccb7610a7b3fe70cfa3
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-29 13:53:59 +0000

    entry point changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'
 import AppPrimaryButton from './elements/atoms/AppPrimaryButton'
 import AppSecondaryButton from './elements/atoms/AppSecondaryButton'
+import NomineeListDropDown from './elements/molecules/NomineeListDropDown';
 
 function App() {
   const queryClient: QueryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="h-screen mx-0 my-0 xy-center-children space-x-2">
-        <AppPrimaryButton> Submit</AppPrimaryButton>
-        <AppSecondaryButton>Cancel</AppSecondaryButton>
+      <div className="h-screen mx-0 my-0 xy-center-children space-x-4 space-y-5 flex-col">
+        <NomineeListDropDown/>
+        <div className='space-y-3 '>
+          <AppPrimaryButton> Submit</AppPrimaryButton>
+          <AppSecondaryButton>Cancel</AppSecondaryButton>
+        </div>
       </div>
     </QueryClientProvider>
   )
