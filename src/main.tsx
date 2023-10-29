7c76385172c861503988a8e30cdc280b02492354
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-29 20:39:51 +0000

    routing added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './elements/pages/Home.tsx';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+const queryClient: QueryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen mx-0 my-0 px-5 py-0">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
